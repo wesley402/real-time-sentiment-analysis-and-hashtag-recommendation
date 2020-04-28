6adc6d1ea130507aa10cbf8b23da2e1385622a81
Kuo-wei Chung <mr.chung402@gmail.com>
2020-04-28 17:42:03 -0400

    first commit

---
+++ src/main/java/com/wesley/app/RealTimeSentimentAnalysisAndHashtagRecommendationApplication.java
@@ -0,0 +1,21 @@
+package com.wesley.app;
+
+
+import java.util.Arrays;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
+
+
+
+@SpringBootApplication
+public class RealTimeSentimentAnalysisAndHashtagRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeSentimentAnalysisAndHashtagRecommendationApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/wesley/app/RealTimeSentimentAnalysisAndHashtagRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesley.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeSentimentAnalysisAndHashtagRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
